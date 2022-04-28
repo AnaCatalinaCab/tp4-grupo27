114975224599f45f8818e5c548121634eba7d278
Lucas5522 <luceitormaser@gmail.com>
2022-04-28 12:05:42 -0300

    commit ejercicio4 tp4

---
+++ TP4_Grupo27/src/main/java/ar/edu/uju/fi/Tp4Grupo27Application.java
@@ -0,0 +1,13 @@
+package ar.edu.uju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4Grupo27Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4Grupo27Application.class, args);
+	}
+
+}
